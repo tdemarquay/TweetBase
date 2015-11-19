1db0e4097f8ac647f6d5db1fa1dfe44a06da4a96
Thibault <thibault@sd-41758.dedibox.fr>
2015-11-19 10:43:10 +0100

    Improved PHP/Pythn connection

--- website/createTask.php
+++ website/createTask.php
@@ -1,32 +1,40 @@
+<a href="listTasks.php"> List tasks</a><br/><br/>
 <?php
 ini_set('display_errors',1);
+
+
 if(isset($_POST['keywords']))
 {
-	$command = "python /home/thibault/tweetBase/TweetBase/test.py test > /dev/null 2>/dev/null &";
+
+	$track = $_POST['keywords'];
+	$parameters = "'".$track."'";
+
+	if(isset($_POST['future']) && $_POST['future']=="future")
+	{	
+		$command = "python /home/thibault/tweetBase/TweetBase/test.py ".$parameters." > /dev/null 2>/dev/null &";
+	}
+	else $command ="";
+
+	echo "The executed command is  : ".$command;
 	exec("pkill python");
-	exec($command,$output);
-	print_r($output);
-	print_r($_POST);
+	if(!empty($command))exec($command,$output);
+	//print_r($output);
+	//print_r($_POST);
 }
 ?>
 
-?>
-<a href="listTasks.php"> List tasks</a><br/><br/>
 
 <h1> Create task</h1>
 <div style="text-align:center">
 	<form method="post" action="createTask.php">
 	   <p>
 		   <label for="pseudo">Keywords :</label>
-		   <input type="text" name="keywords" id="pseudo" />
+		   <input style="width:500px" type="text" name="keywords" id="pseudo" /><br/>Separate by comma (=OR). <br/>Can have two words or more between two commas (=AND). <br/>Can be a hastag (don't forget the #)
 		   
-		   <br /><br />
-		   <label for="pass">Hashtags :</label>
-		   <input type="text" name="hashtags" id="pass" />
 
 		   <br /><br />
 		   <label for="pass">Future tweets :</label>
-		   <input type="checkbox" name="future" value="future" checked disabled> 
+		   <input type="checkbox" name="future" value="future" checked> 
 		  
 		   <br /><br />
 		   <input type="submit" name="create" value="Create task" id="pass" />
